c72e2811f505883ba7127c7789fa7e8d435f723c
Saad Mohammad Shibli <saadshibli27@gmail.com>
2025-07-24 01:08:11 +0530

    backendURL

--- frontend/src/components/Course.jsx
+++ frontend/src/components/Course.jsx
@@ -10,7 +10,7 @@ function Course() {
     useEffect(() => {
         const getBook = async () => {
             try {
-                const res = await axios.get("http://localhost:4001/book");
+                const res = await axios.get("https://bookhavenapp.onrender.com/book");
                 setBook(res.data);
                 console.log(res.data);
             } catch (error) {
--- frontend/src/components/Signup.jsx
+++ frontend/src/components/Signup.jsx
@@ -24,7 +24,7 @@ function Signup() {
             email: data.email,
             password: data.password,
         }
-        await axios.post("http://localhost:4001/user/signup", userInfo)
+        await axios.post("https://bookhavenapp.onrender.com/user/signup", userInfo)
             .then((res) => {
                 console.log(res.data);
                 if(res.data) {
